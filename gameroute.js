e1b0117716801502f1e17be56dafb3c528db1f20
gabchia98 <gabchia98@gmail.com>
2018-04-09 11:04:52 +0800

    Added documentations and renaming of some files

--- gameroute.js
+++ gameroute.js
@@ -1,3 +1,3 @@
-function loadIndex() {
-    game = window.open("index.html", "_self");
+function loadGame() {
+    game = window.open("game.html", "_self"); // when the Play Single Player button is clicked in the index.html, this function will open the game.html in the same tab / window.
 }
